c43bdb152f8cf2d5b7af1489c4c05f86f9a0b12e
sara-almqvist <sara_almqvist@hotmail.com>
2026-06-05 12:15:56 +0200

    Build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/projekt_webshop_2026/',
   plugins: [react(), tailwindcss()],
 });
